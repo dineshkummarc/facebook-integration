5c4e1c58e3e353e77d281e88b4dacc55c87e3444
Mohamed Elsaed <m.ashraf@converted.in>
2024-07-24 15:57:36 +0300

    update namespaces

--- src/services/Facebook/Accounts/ConnectUserAccountsService.php
+++ src/services/Facebook/Accounts/ConnectUserAccountsService.php
@@ -3,13 +3,25 @@
 namespace Cvtdin\ConvertedinFacebook\services\Facebook\Accounts;
 
 use Cvtdin\ConvertedinFacebook\DataObjects\BusinessDataObject;
+use FacebookAds\Api;
 use FacebookAds\Object\Business;
 
 class ConnectUserAccountsService
 {
+
+    private static Api $api;
+
     public static function businesses(BusinessDataObject $businessDataObject)
     {
+        self::init(); // Ensure the Api instance is initialized
         $business = new Business();
-        return$business::getFieldsEnum();
+        return $business::getFieldsEnum();
+    }
+
+    public static function init()
+    {
+        if (is_null(self::$api)) {
+            self::$api = app('facebook-api');
+        }
     }
 }
